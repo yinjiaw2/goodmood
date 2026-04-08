84134cf52cb943e54f5089e6c011296a7de572fd
nicole-nxn <nicolenxn1234@gmail.com>
2026-04-08 17:26:20 +1000

    move

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import ServiceProcess from "@/components/home/ServiceProcess";
 import SuccessCases from "@/components/home/SuccessCases";
 import FAQ from "@/components/home/Faq";
 import Contact from "@/components/home/Contact";
-import HomeSectionTracker from "@/components/home/HomeSectionTracker";
+import HomeSectionTracker from "@/analytics/HomeSectionTracker";
 
 export default function Home() {
   return (
