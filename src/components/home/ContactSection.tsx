ed73b643a732ae3d60e06b79a424cd8d04c3f101
nicole-nxn <nicolenxn1234@gmail.com>
2026-04-28 16:24:35 +1000

    fix form on mobile

--- src/components/home/ContactSection.tsx
+++ src/components/home/ContactSection.tsx
@@ -15,8 +15,8 @@ export default function ContactSection() {
       id="contact"
       className="w-full bg-[#1A1A1A] px-6 py-24 text-white md:px-10 lg:px-16"
     >
-      <div className="mx-auto max-w-7xl gap-12 flex flex-row items-center">
-        <div className="flex flex-col gap-6  w-3/7">
+      <div className="mx-auto flex max-w-7xl flex-col gap-12 lg:flex-row lg:items-center">
+        <div className="flex w-full flex-col gap-6 lg:w-3/7">
           <span
             className="inline-flex w-fit rounded-full border px-4 py-2 text-sm font-semibold uppercase tracking-widest"
             style={{
